96e09e905d854ed8bee34170b3764a9c5734982c
Chris Wininger <cwininger@sonatype.com>
2022-04-11 09:32:42 -0400

    first commit

---
+++ src/main/java/LiftTestHasStrutsVuln.java
@@ -0,0 +1,6 @@
+public class LiftTestHasStrutsVuln
+{
+    public static void main(final String[] args) {
+        System.out.println("Hello World, I have vulnerabilities. It's best to be honest with yourself.");
+    }
+}
